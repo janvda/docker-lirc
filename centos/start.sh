23a75f2e7c74633ca26cf4d74afde37d7f45c15c
Jan Van den Audenaerde <jan.vandenaudenaerde@gmail.com>
2021-05-22 12:17:33 +0200

    changes should work in combination with node-red-RC-controller

--- centos/start.sh
+++ centos/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -v
 [[ -e "/var/lirc_raw_data/lirc0_events" ]] || mkfifo /var/lirc_raw_data/lirc0_events
-[[ -e "/var/lirc_raw_data/lirc0_errors" ]] || mkfifo /var/lirc_raw_data/lirc0_errors
 
-#mode2 --driver default --device /dev/lirc0 > /var/lirc_raw_data/lirc0_events 2> /var/lirc_raw_data/lirc0_errors
+mode2 --driver default --device /dev/lirc0 > /var/lirc_raw_data/lirc0_events
 
-sleep 36000
+#sleep 36000
